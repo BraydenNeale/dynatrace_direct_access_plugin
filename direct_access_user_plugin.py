bf5514ad16a6bcf0e584759622de8e118feaa90f
Brayden Neale <Brayden.Neale@dynatrace.com>
2018-06-10 17:03:18 +1000

    Add basic error reporting to DT plugin

--- direct_access_user_plugin.py
+++ direct_access_user_plugin.py
@@ -13,10 +13,29 @@ class DirectAccessUserPlugin(BasePlugin):
 
         json_file_path = "D:\\Software\\Scripts\\DirectAccessStats.JSON"
         stats = {}
-        with open(json_file_path, encoding='utf-8') as json_file:
-            clean_file = json_file.read().replace('\ufeff', '')
-            stats = json.loads(clean_file)
         
+        try:
+            with open(json_file_path, encoding='utf-8') as json_file:
+                clean_file = json_file.read().replace('\ufeff', '')
+
+                try:
+                    stats = json.loads(clean_file)
+                except ValueError as ex:
+                    raise ConfigException('Unable to parse "%s" as JSON' % json_file_path) from ex
+        except IOError as ex:
+            raise ConfigException('Could not open file "%s"' % json_file_path) from ex
+        '''
+        # Test Data
+        stats['TotalConnections'] = random.randint(1,101)
+        stats['TotalDAConnections'] = random.randint(1,101)
+        stats['TotalVpnConnections'] = random.randint(1,101)
+        stats['TotalUniqueUsers'] = random.randint(1,101)
+        stats['MaxConcurrentConnections'] = random.randint(1,101)
+        stats['TotalCumulativeConnections'] = random.randint(1,101)
+        stats['TotalBytesIn'] = random.randint(1,101)
+        stats['TotalBytesOut'] = random.randint(1,101)
+        stats['TotalBytesInOut'] = random.randint(1,101)
+        '''
         self.results_builder.absolute(key='total_connections', value=stats['TotalConnections'], entity_id=pgi_id)
         self.results_builder.absolute(key='total_DA_connections', value=stats['TotalDAConnections'], entity_id=pgi_id)
         self.results_builder.absolute(key='total_vpn_connections', value=stats['TotalVpnConnections'], entity_id=pgi_id)
