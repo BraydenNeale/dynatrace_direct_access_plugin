74992b17a85bbd786b8f2c1221414d4dd6c8d38b
Brayden Neale <Brayden.Neale@dynatrace.com>
2018-06-25 13:19:10 +1000

    Simplify UI display - We care about custom charting and not host metric displays: Remove zip - this is now tracked internally

--- direct_access_user_plugin.py
+++ direct_access_user_plugin.py
@@ -2,20 +2,19 @@ import subprocess
 import json
 import logging
 import tempfile
-import random
+#import random
 from ruxit.api.base_plugin import BasePlugin
 from ruxit.api.snapshot import pgi_name
 from ruxit.api.exceptions import ConfigException
 
 class DirectAccessUserPlugin(BasePlugin):
     def query(self, **kwargs):
-        #pgi = self.find_single_process_group(pgi_name('DirectAccess'))
-        pgi = self.find_single_process_group(pgi_name('oneagent_sdk.demo_app'))
+        pgi = self.find_single_process_group(pgi_name('DirectAccess'))
         pgi_id = pgi.group_instance_id
 
         json_file_path = "D:\\Software\\Scripts\\DirectAccessStats.JSON"
         stats = {}
-        '''
+        
         try:
             with open(json_file_path, encoding='utf-8') as json_file:
                 clean_file = json_file.read().replace('\ufeff', '')
@@ -26,6 +25,7 @@ class DirectAccessUserPlugin(BasePlugin):
                     raise ConfigException('Unable to parse "%s" as JSON' % json_file_path) from ex
         except IOError as ex:
             raise ConfigException('Could not open file "%s"' % json_file_path) from ex
+            
         '''
         # Test Data
         stats['TotalConnections'] = random.randint(1,101)
@@ -37,6 +37,7 @@ class DirectAccessUserPlugin(BasePlugin):
         stats['TotalBytesIn'] = random.randint(1,101)
         stats['TotalBytesOut'] = random.randint(1,101)
         stats['TotalBytesInOut'] = random.randint(1,101)
+        '''
         
         self.results_builder.absolute(key='total_connections', value=stats['TotalConnections'], entity_id=pgi_id)
         self.results_builder.absolute(key='total_DA_connections', value=stats['TotalDAConnections'], entity_id=pgi_id)
